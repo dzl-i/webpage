eeba4a1bd0815d7616e02aea29753cdbf867e42e
dzl-i <denzel.iskandar@gmail.com>
2023-05-05 01:29:12 +0700

    Portfolio Website

---
+++ src/assets/index.js
@@ -0,0 +1,66 @@
+import lofi from "./lofi.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import c from "./tech/c.png";
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import java from "./tech/java.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import postgresql from "./tech/postgresql.png";
+import python from "./tech/python.png";
+
+import codecademy from "./company/codecademy.png";
+import uniprep from "./company/uniprep.png";
+import unsw1 from "./company/unsw1.png";
+import unsw2 from "./company/unsw2.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  lofi,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  postgresql,
+  c,
+  java,
+  python,
+  codecademy,
+  uniprep,
+  unsw1,
+  unsw2,
+  carrent,
+  jobit,
+  tripguide,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,254 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  codecademy,
+  uniprep,
+  unsw1,
+  unsw2,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  postgresql,
+  c,
+  java,
+  python,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Education",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Frontend Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Cybersecurity Enthusiast",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "C",
+    icon: c,
+  },
+  {
+    name: "Java",
+    icon: java,
+  },
+  {
+    name: "Python",
+    icon: python,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "PostgreSQL",
+    icon: postgresql,
+  },
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+];
+
+const experiences = [
+  {
+    title: "Physical Science Stream",
+    company_name: "UNSW Foundation Studies",
+    icon: uniprep,
+    iconBg: "#ffffff",
+    date: "October 2020 - July 2021",
+    points: [
+      "University Preparation - Physics, Chemistry, Advanced Mathematics",
+    ],
+  },
+  {
+    title: "Bachelor of Software Engineering",
+    company_name: "University of New South Wales",
+    icon: unsw1,
+    iconBg: "#ffe600",
+    date: "September 2021 - Present",
+    points: [
+      "Programming Fundamentals in C",
+      "Assembly Language in MIPS",
+      "Basic File Systems in C",
+      "Backend Development in Javascript and Typescript",
+      "Data Structures and Algorithms in C",
+      "Backend Development and Object-Oriented Programming in Java",
+      "Database Systems in PostgreSQL",
+      "Software Verification in Dafny",
+      "Full-stack Development in Python, ReactJS, PostgreSQL and AWS Elasticbeanstalk",
+      "Socket Programming in Python",
+    ],
+  },
+  {
+    title: "Front-End Engineer",
+    company_name: "Codecademy",
+    icon: codecademy,
+    iconBg: "#204056",
+    date: "December 2022 - Present",
+    points: [
+      "Improved Styling with CSS",
+      "Make Interactive Websites",
+      "Build Responsive Websites",
+      "Async Javascript and HTTP Requests",
+      "React and Redux",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
